569fb7b3fbeece125232b284eae37b878b0fc7bb
Akshra Singh <akshras479@gmail.com>
2026-07-18 23:36:39 +0530

    Debug forgot password email service

--- Backend/src/utils/sendEmail.js
+++ Backend/src/utils/sendEmail.js
@@ -3,6 +3,12 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log(
+  "EMAIL_PASS:",
+  process.env.EMAIL_PASS ? "Loaded ✅" : "Missing ❌"
+);
+
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
@@ -12,12 +18,16 @@ const transporter = nodemailer.createTransport({
 });
 
 const sendEmail = async (options) => {
-  await transporter.sendMail({
+  console.log("Sending mail to:", options.email);
+
+  const info = await transporter.sendMail({
     from: `"CampusCart" <${process.env.EMAIL_USER}>`,
     to: options.email,
     subject: options.subject,
     html: options.message,
   });
+
+  console.log("MAIL SENT:", info.response);
 };
 
 export default sendEmail;
